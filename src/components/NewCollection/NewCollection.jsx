6c4f06bc0e05b56723afff15543a9b54d09052c3
Your Name <your.email@example.com>
2025-05-21 09:16:28 +0300

    feat: connect NewCollection to Redux store and add BEM SCSS styles

--- src/components/NewCollection/NewCollection.jsx
+++ src/components/NewCollection/NewCollection.jsx
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
 import styles from "./NewCollection.module.scss";
 
 const NewCollection = () => {
+  const newCollection = useSelector((state) => state.content.newCollection);
+
   return (
     <section className={styles["new-collection"]}>
-      {/* Контент NewCollection */}
+      <h2 className={styles["new-collection__title"]}>New Collection</h2>
+      <div className={styles["new-collection__items"]}>
+        {newCollection.map((item) => (
+          <div key={item.id} className={styles["new-collection__item"]}>
+            <img
+              src={item.image}
+              alt={item.title}
+              className={styles["new-collection__img"]}
+            />
+            <div className={styles["new-collection__name"]}>{item.title}</div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
