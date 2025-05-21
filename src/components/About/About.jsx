500962da0219b8b83f87a0cf744cb0006514d0b0
Your Name <your.email@example.com>
2025-05-21 09:41:22 +0300

    feat: connect About to Redux store and add BEM SCSS styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,24 @@
+import { useSelector } from "react-redux";
 import styles from "./About.module.scss";
 
 const About = () => {
-  return <section className={styles["about"]}>{/* Контент About */}</section>;
+  const about = useSelector((state) => state.content.about);
+
+  return (
+    <section className={styles["about"]}>
+      <div className={styles["about__img-wrap"]}>
+        <img
+          src={about.image}
+          alt={about.title}
+          className={styles["about__img"]}
+        />
+      </div>
+      <div className={styles["about__content"]}>
+        <h2 className={styles["about__title"]}>{about.title}</h2>
+        <p className={styles["about__text"]}>{about.text}</p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
