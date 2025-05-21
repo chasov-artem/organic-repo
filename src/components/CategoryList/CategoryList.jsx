9a7f6e25e3d2932c2001397fb8883be76c51bd67
Your Name <your.email@example.com>
2025-05-21 10:38:06 +0300

    cleanup unused CSS, update global styles and components

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
 import styles from "./CategoryList.module.scss";
 
 const CategoryList = () => {
+  const categories = useSelector((state) => state.content.categories);
+
   return (
     <section className={styles["category-list"]}>
-      {/* Контент CategoryList */}
+      <h2 className={styles["category-list__title"]}>Shop by Categories</h2>
+      <div className={styles["category-list__items"]}>
+        {categories.map((cat) => (
+          <div key={cat.id} className={styles["category-list__item"]}>
+            <img
+              src={cat.image}
+              alt={cat.title}
+              className={styles["category-list__img"]}
+            />
+            <div className={styles["category-list__name"]}>{cat.title}</div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
 import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <header className={styles["header"]}>{/* Контент Header */}</header>;
+  const { logo, nav } = useSelector((state) => state.content.header);
+
+  return (
+    <header className={styles["header"]}>
+      <div className={styles["header__logo"]}>{logo}</div>
+      <nav className={styles["header__nav"]}>
+        {nav.map((item, idx) => (
+          <a key={idx} href="#" className={styles["header__nav-item"]}>
+            {item}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
