272988e8fc551f40848e41d424ead5725fb1b11e
Your Name <your.email@example.com>
2025-05-25 16:45:54 +0300

    styles: categoryList start

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -3,13 +3,12 @@ import styles from "./CategoryList.module.scss";
 
 const CategoryList = () => {
   const categories = useSelector((state) => state.content.categories);
-  const stripe = categories[categories.length - 1];
 
   return (
     <section className={styles["category-list"]}>
       <h2 className={styles["category-list__title"]}>Shop by Categories</h2>
       <div className={styles["category-list__items"]}>
-        {categories.slice(0, 5).map((cat, idx) => (
+        {categories.slice(0, 4).map((cat, idx) => (
           <div key={cat.id} className={styles["category-list__item"]}>
             <picture>
               <source
@@ -27,14 +26,6 @@ const CategoryList = () => {
             </picture>
           </div>
         ))}
-      </div>
-      <div className={styles["category-list__stripe-img-wrap"]}>
-        <img
-          src={stripe.image}
-          srcSet={`${stripe.image2x} 2x`}
-          alt={stripe.title}
-          className={styles["category-list__stripe-img"]}
-        />
       </div>
     </section>
   );
