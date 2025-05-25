a5eb300b21acab0ca94fee424691066ca8ffb3bd
Your Name <your.email@example.com>
2025-05-25 11:47:53 +0300

    photos: categoryList

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -9,14 +9,22 @@ const CategoryList = () => {
     <section className={styles["category-list"]}>
       <h2 className={styles["category-list__title"]}>Shop by Categories</h2>
       <div className={styles["category-list__items"]}>
-        {categories.slice(0, 5).map((cat) => (
+        {categories.slice(0, 5).map((cat, idx) => (
           <div key={cat.id} className={styles["category-list__item"]}>
-            <img
-              src={cat.image}
-              srcSet={`${cat.image2x} 2x`}
-              alt={cat.title}
-              className={styles["category-list__img"]}
-            />
+            <picture>
+              <source
+                srcSet={`/mob/shop${idx + 1}.png 1x, /mob/shop${
+                  idx + 1
+                }-2x.png 2x`}
+                media="(max-width: 991px)"
+              />
+              <img
+                src={cat.image}
+                srcSet={`${cat.image2x} 2x`}
+                alt={cat.title}
+                className={styles["category-list__img"]}
+              />
+            </picture>
           </div>
         ))}
       </div>
